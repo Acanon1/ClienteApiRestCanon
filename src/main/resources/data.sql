9c4cd26c3438e82d5970629c1cc8107ee24cad1d
anthonycanon <20180343@aloe.ulima.edu.pe>
2024-01-30 21:11:16 -0500

    correcion data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO CLIENTE (nombre, apellido, anios) VALUES
-    ('John', 'Doe', '12'),
-    ('Jane', 'Smith', '18'),
-    ('Bob', 'Johnson', '22');
+    ('John', 'Doe', 12),
+    ('Jane', 'Smith', 18),
+    ('Bob', 'Johnson', 22);
